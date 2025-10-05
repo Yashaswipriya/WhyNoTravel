e90feca83fd28178434253a27e5e12c9dc44941f
Yashaswipriya <yashaswipulukuri@gmail.com>
2025-10-05 23:31:34 +0530

    added liquid ether background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import { Righteous } from "next/font/google";
+import LiquidEther from "@/components/LiquidEther";
 
 const righteous = Righteous({ subsets: ["latin"], weight: "400" });
 
@@ -19,14 +20,34 @@ export default function Hero() {
 
   return (
     <section ref={ref} className="relative h-[170vh] bg-black text-white overflow-hidden">
+        {/* LiquidEther Background */}
+      <div className="absolute inset-0 z-0">
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
       {/* Hero Text */}
-      <div className="sticky top-0 flex flex-col justify-center items-center h-screen text-center z-10">
-        <h1 className={`text-[12vw] leading-[1.1] ${righteous.className}`}>
+      <div className="sticky top-0 flex flex-col justify-center items-center h-screen text-center z-10 pointer-events-none">
+        <h1 className={`text-[15vw] leading-[1.1] ${righteous.className}`}>
           WHY
         </h1>
 
         <h1
-          className={`text-[12vw] leading-[1.1] ${righteous.className} flex justify-center items-center`}
+          className={`text-[15vw] leading-[1.1] ${righteous.className} flex justify-center items-center`}
         >
           N
           <motion.span
@@ -37,7 +58,7 @@ export default function Hero() {
           </motion.span>
         </h1>
 
-        <h1 className={`text-[12vw] leading-[1.1] ${righteous.className}`}>
+        <h1 className={`text-[15vw] leading-[1.1] ${righteous.className}`}>
           TRAVEL
         </h1>
 
